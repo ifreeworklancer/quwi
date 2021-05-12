bdb577f60083be1e647c31395ce9f1a9b015060c
Alex Yakushov <yakushov_as@groupbwt.com>
2021-05-12 13:53:07 +0300

    feat: created template page login

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
@@ -24,32 +24,35 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+.container {
+  width: 100%;
+  max-width: 100%;
+  padding: 0 15px;
+
+  @media (min-width: 576px) {
+    max-width: 540px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1140px;
+  }
+
+  @media (min-width: 1400px) {
+    max-width: 1320px;
+  }
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.page-title {
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 25px;
 }
 </style>
---
+++ pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="auth">
+    <div class="container">
+      <el-row>
+        <el-col :gutter="15" :span="24">
+          <h1 class="page-title">Login</h1>
+        </el-col>
+      </el-row>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.auth {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+
+  .page-title {
+    text-align: center;
+  }
+}
+</style>
